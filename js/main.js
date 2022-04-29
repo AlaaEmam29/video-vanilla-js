6eec0d8f643eb9bb2bb9839e1b337d7dd3c08b8d
AlaaShaban29 <alaashaban29_sd@fcis.edu.eg>
2022-04-29 16:18:07 +0200

    add

---
+++ js/main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const switchBtn = document.querySelector(".switch-btn"),
+video = document.querySelector(".video-container");
+switchBtn.addEventListener("click",function() {
+// const btn = e.currentTarget.classList;
+if(switchBtn.classList.contains("slide"))
+{
+    switchBtn.classList.remove("slide");
+    video.play();
+}
+else
+{
+    switchBtn.classList.add("slide");
+    video.pause();
+ 
+}
+})
+window.addEventListener("load",function() {
+    document.querySelector(".loading-screen").classList.add("hide-screen")
+})
